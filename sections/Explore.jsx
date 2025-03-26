79f0f527e93f702a1e1c2281623cc24aa49c23f9
FrostFoe <131578101+FrostFoe7@users.noreply.github.com>
2025-03-26 15:54:01 +0000

    m

--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -20,9 +20,9 @@ const Explore = () => {
         viewport={{ once: false, amount: 0.25 }}
         className={`${styles.innerWidth} mx-auto flex flex-col`}
       >
-        <TypingText title="| The World" textStyles="text-center" />
+        <TypingText title="| আমাদের তৈরি কিছু টেমপ্লেট " textStyles="text-center" />
         <TitleText
-          title={<>Choose the world you want <br className="md:block hidden" /> to explore</>}
+          title={<>খুঁজে নিন আপনার পছন্দের ডিজাইন <br className="md:block hidden" /> যা মুগ্ধ করতে বাধ্য সবাইকে</>}
           textStyles="text-center"
         />
         <div className="mt-[50px] flex lg:flex-row flex-col min-h-[70vh] gap-5">
